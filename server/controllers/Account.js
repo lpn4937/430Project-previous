0a480164a8feca79e2de3f23c094749e258ec74e
Lucas Nichols <lpn4937@rit.edu>
2019-03-23 19:06:07 -0400

    started part d

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -3,11 +3,7 @@ const models = require('../models');
 const Account = models.Account;
 
 const loginPage = (req, res) => {
-  res.render('login', {csrfToken: req.csrfToken()});
-};
-
-const signupPage = (req, res) => {
-  res.render('signup', {csrfToken: req.csrfToken()});
+  res.render('login', { csrfToken: req.csrfToken() });
 };
 
 const logout = (req, res) => {
@@ -79,8 +75,19 @@ const signup = (request, response) => {
   });
 };
 
+const getToken = (request, response) => {
+  const req = request;
+  const res = response;
+
+  const csrfJSON = {
+    csrfToken: req.csrfToken(),
+  };
+
+  res.json(csrfJSON);
+};
+
 module.exports.loginPage = loginPage;
 module.exports.login = login;
 module.exports.logout = logout;
-module.exports.signupPage = signupPage;
 module.exports.signup = signup;
+module.exports.getToken = getToken;
--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -9,7 +9,7 @@ const makerPage = (req, res) => {
       return res.status(400).json({ error: 'An error occurred' });
     }
 
-    return res.render('app', {csrfToken: req.csrfToken(), domos: docs });
+    return res.render('app', { csrfToken: req.csrfToken(), domos: docs });
   });
 };
 
@@ -42,6 +42,21 @@ const makeDomo = (req, res) => {
   return domoPromise;
 };
 
+const getDomos = (request, response) => {
+  const req = request;
+  const res = response;
+
+  return Domo.DomoModel.findByOwner(req.session.account._id, (err,docs) => {
+    if(err){
+      console.log(err);
+      return res.status(400).json({error: 'An error occurred'})
+    }
+
+    return res.json({domos: docs});
+  });
+};
+
 
 module.exports.makerPage = makerPage;
+module.exports.getDomos = getDomos;
 module.exports.make = makeDomo;
